616645a1bcfde8cce83eb43254f1d5102ee81fbc
kintansemi <kintansemi@gmail.com>
2021-09-14 15:21:39 +0700

    Tambah Pengurangan

--- src/mcc5657/MCC5657.java
+++ src/mcc5657/MCC5657.java
@@ -18,7 +18,8 @@ public class MCC5657 {
         // TODO code application logic here
         Pembagian pembagian = new Pembagian();
         System.out.println(pembagian.bagi(4, 2));
-        
+        Pengurangan p = new Pengurangan();
+        System.out.println("Hasil Pengurangan = " + p.Hitung(454, 35));
     }
     
 }
---
+++ src/mcc5657/Pengurangan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mcc5657;
+
+/**
+ *
+ * @author kintan semi
+ */
+public class Pengurangan {
+    int a;
+    int b;
+    
+    public int Hitung(int a, int b){
+        int hasil;
+        hasil = a - b;
+        return hasil;
+    }
+
+}
