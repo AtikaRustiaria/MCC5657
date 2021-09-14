c06bf7810526febb9b4134c97daccbe0e0c2a43a
dyas irvan <dyas.mail@gmail.com>
2021-09-14 14:46:55 +0700

    Fitur Pembagian

--- src/mcc5657/MCC5657.java
+++ src/mcc5657/MCC5657.java
@@ -16,6 +16,9 @@ public class MCC5657 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Pembagian pembagian = new Pembagian();
+        System.out.println(pembagian.bagi(4, 2));
+        
     }
     
 }
